a64611d1225489396256cff7e824340f4070f9a6
lohith <lohithreddy.e@gmail.com>
2023-08-07 14:36:24 +0530

    added dotenv package and created a environment variables

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+if (process.env.NODE_ENV.toLowerCase().trim() === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello world!</h1>');
